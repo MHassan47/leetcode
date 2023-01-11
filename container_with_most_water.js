df1fcf6b1e89b02187772b39dabfed9718c33dd5
Mohamed <mhassankd35@gmail.com>
2023-01-10 20:56:52 -0600

    adds container with most water leetcode problem

---
+++ container_with_most_water.js
@@ -0,0 +1,21 @@
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+const maxArea = function (height) {
+  let area = 0
+    let i = 0
+    let j = height.length-1;
+  while (i < j) {
+    const possibleMax = (j-i) * Math.min(height[i], height[j]);
+    area = Math.max(area, possibleMax);
+    if (height[i] > height[j]) {
+      j-=1;
+    }else{
+    i+=1;
+    }
+  }
+  return area;
+};
