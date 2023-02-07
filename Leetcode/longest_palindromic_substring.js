22abe7ae130c050a4f717e5747adceb836ed5415
Mohamed <mhassankd35@gmail.com>
2023-02-07 01:08:03 -0600

    fixes longest palindrome coderbyte solution

--- Leetcode/longest_palindromic_substring.js
+++ Leetcode/longest_palindromic_substring.js
@@ -5,26 +5,28 @@
 // - loop that will loop in either direction of middle Point
 // - function that will run the loops on either end
 
-const longestPalindrome = function (s) {
-  const longestPalindrome = "";
+const longestPalindrome = function(s) {
+  let longestPalindrome = "";
 
-  const oddPalindrome = expandFromCenter(s, i, i);
-  const evenPalindrome = expandFromCenter(s, i - 1, i);
+for (let i = 0; i < s.length; i++){
+let oddPalindrome = expandFromCenter(s, i, i);
+let evenPalindrome = expandFromCenter(s, i - 1, i);
 
-  if (oddPalindrome.length > longestPalindrome.length) {
-    longest = oddPalindrome;
-  }
-  if (evenPalindrome.length > longestPalindrome.length) {
-    longest = evenPalindrome;
-  }
-  return longestPalindrome;
+if (oddPalindrome.length > longestPalindrome.length) {
+longestPalindrome = oddPalindrome;
+}
+if (evenPalindrome.length > longestPalindrome.length) {
+longestPalindrome = evenPalindrome;
+}
+}
+return longestPalindrome;
 };
 
-const expandFromCenter = function (str, left, right) {
-  let i = 0;
-  while (str[left[i]] && str[left - i] === str[right + i]) {
-    i++;
-  }
-  i--;
-  return str.slice(left - i, right + i + 1);
+function expandFromCenter(str, left, right) {
+let i = 0;
+while (str[left -i] && str[left - i] === str[right+i]) {
+i++;
+}
+i--;
+return str.slice(left - i, right + i + 1);
 };
