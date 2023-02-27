064c695b4f0152128d97a51cd5a23d465a3b2bfd
Mohamed <mhassankd35@gmail.com>
2023-02-26 21:08:04 -0600

    adds number search coderbyte  solution

--- Coderbyte/number_search.js
+++ Coderbyte/number_search.js
@@ -6,28 +6,44 @@ letters in the string. 32 / 17 = 1.882, and the final answer should be rounded t
 so the answer is 2. Only single digit numbers separated by spaces will be used throughout 
 the whole string (So this won't ever be the case: hello44444 world). Each string will also have at least one letter. */
 
+const letterSearch = (str) => {
+  let letters = /[a-zA-Z]/g;
+  let numbers = /[0-9]/g;
+  let lettersCount = 0;
+  let numbersCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(letters)) {
+      lettersCount++;
+    }
+    if (str[i].match(numbers)) {
+      numbersCount += Number(str[i]);
+    }
+  }
+  return Math.round(numbersCount / lettersCount);
+};
+
 // regular express only numbers
 // regular expression only alphabets
 // loop through
 // track counter variable
 
-const numberSearch = (str) => {
-  let sumofNumbers = 0;
-  let sumofAlphabets = 0;
-  let numbers = /[0-9]/;
-  let alphabets = /[a-z]/gi;
+// const numberSearch = (str) => {
+//   let sumofNumbers = 0;
+//   let sumofAlphabets = 0;
+//   let numbers = /[0-9]/;
+//   let alphabets = /[a-z]/gi;
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].match(numbers)) {
-      sumofNumbers += Number(str[i]);
-      //   console.log(str[i]);
-    }
-    if (str[i].match(alphabets)) {
-      sumofAlphabets++;
-      console.log(str[i]);
-    }
-  }
-  return Math.round(sumofNumbers / sumofAlphabets);
-};
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i].match(numbers)) {
+//       sumofNumbers += Number(str[i]);
+//       //   console.log(str[i]);
+//     }
+//     if (str[i].match(alphabets)) {
+//       sumofAlphabets++;
+//       console.log(str[i]);
+//     }
+//   }
+//   return Math.round(sumofNumbers / sumofAlphabets);
+// };
 
-console.log(numberSearch("Hello6 9World 2, Nic8e D7ay"));
+console.log(letterSearch("Hello6 9World 2, Nic8e D7ay"));
