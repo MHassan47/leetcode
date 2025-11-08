98954f768e47df8cb14f33d494fbdb4c281704d8
Mohamed <mhassankd35@gmail.com>
2025-11-07 23:35:50 -0600

    adds contiguous subarray coderbyte solutions

--- Coderbyte/max_subarray.js
+++ Coderbyte/max_subarray.js
@@ -4,18 +4,26 @@
  * the contiguous subarray [4,−1,2,1] has the largest sum = 6.
  */
 
-// sliding window
-// loop across array
-// variable that stores the max sum
+// const largest = (arr) => {
+//   let largest = 0;
 
-const maxSubarray = (array) => {
-  let largestSum = array[0];
-  let currentSum = array[0];
-  for (let i = 0; i < array.length; i++) {
-    currentSum = Math.max(currentSum + array[i], array[i]);
-    largestSum = Math.max(currentSum, largestSum);
-  }
-  return largestSum;
-};
+//   let possible = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     possible = Math.max(possible + arr[i], arr[i]);
+//     largest = Math.max(possible, largest);
+//   }
 
-console.log(maxSubarray([-2, 1, -3, 4, -1, 2, -6, 3, -5, 4]));
+//   return largest;
+// };
+
+// const maxSubarray = (array) => {
+//   let largestSum = array[0];
+//   let currentSum = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     currentSum = Math.max(currentSum + array[i], array[i]);
+//     largestSum = Math.max(currentSum, largestSum);
+//   }
+//   return largestSum;
+// };
+
+console.log(largest([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
